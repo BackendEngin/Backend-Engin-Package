45a2026b15c7bc0275e27fdfbdecd1a32e360338
AlirezaSharif1995 <backendengin@gmail.com>
2025-04-03 14:19:14 +0330

    Update TriggerEvent.cs

--- Runtime/Scripts/Services/TriggerEvent.cs
+++ Runtime/Scripts/Services/TriggerEvent.cs
@@ -6,6 +6,7 @@ public class TriggerEvent : Request
 {
     public static async Task<APIResponse> Trigger(string eventName, string eventType)
     {
+        RequestData.user = new User();
         RequestData.requestURL = "/eventManager/logEvent";
         RequestData.requestType = "POST";
         RequestData.schemaID = PlayerPrefs.GetString("schemaID");
